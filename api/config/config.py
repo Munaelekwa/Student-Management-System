9df88b114285c762b50053154b064376d5f8793a
Munachi Elekwa <munaelekwa@gmail.com>
2023-03-15 22:34:40 +0100

    modified soe configurations

--- api/config/config.py
+++ api/config/config.py
@@ -4,10 +4,6 @@ from datetime import timedelta
 
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
-uri = os.environ.get('DATABASE_URL') # or other relevant config var
-if uri and uri.startswith('postgres://'):
-    uri = uri.replace('postgres://', 'postgresql://', 1)
-
 class Config:
     SECRET_KEY = config('SECRET_KEY', 'secret')
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=60)
@@ -27,9 +23,7 @@ class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'sqlite://'
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = uri
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    DEBUG = config('DEBUG', False, cast=bool)
+    pass
     
 config_dict = {
     'dev': DevConfig,
